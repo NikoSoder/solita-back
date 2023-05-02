6a064ece39cbd9b61fa807c735c19105b673d7ac
Niko <soder.n@gmail.com>
2023-05-02 15:21:04 +0300

    Add utils

--- utils/config.js
+++ utils/config.js
@@ -1,17 +1,9 @@
 require("dotenv").config();
 
-let USER = process.env.USER;
-let HOST = process.env.HOST;
-let DATABASE = process.env.DATABASE;
-let PASSWORD = process.env.PASSWORD;
-let DATABASE_PORT = process.env.DATABASE_PORT;
 let PORT = process.env.PORT;
+let DATABASE_URL = process.env.DATABASE_URL;
 
 module.exports = {
-  USER,
-  HOST,
-  DATABASE,
-  PASSWORD,
-  DATABASE_PORT,
   PORT,
+  DATABASE_URL,
 };
---
+++ utils/db.js
@@ -0,0 +1,19 @@
+const Sequelize = require("sequelize");
+const config = require("./config");
+
+const sequelize = new Sequelize(config.DATABASE_URL);
+
+const connectToDatabase = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("database connected");
+  } catch (err) {
+    console.log("connecting database failed");
+    return process.exit(1);
+  }
+
+  return null;
+};
+/* connectToDatabase(); */
+
+module.exports = { connectToDatabase, sequelize };
