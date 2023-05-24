d9ea2f2d124e649ad31b9a832e6097bcd2ae9bac
Niko <soder.n@gmail.com>
2023-05-24 22:44:28 +0300

    Remove useless path

--- controllers/stations.js
+++ controllers/stations.js
@@ -7,13 +7,4 @@ stationsRouter.get("/", async (req, res) => {
   res.json(stations);
 });
 
-stationsRouter.get("/:id", async (req, res) => {
-  req.station = await Station.findByPk(req.params.id);
-  if (req.station) {
-    res.json(req.station);
-  } else {
-    res.status(404).json({ error: "Station not found" });
-  }
-});
-
 module.exports = stationsRouter;
